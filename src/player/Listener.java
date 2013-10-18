93e56b7a760934ad5a86a7fb5384dbedd6ae43ff
Olga Shestopalova <olgash@mit.edu>
2013-10-18 14:40:09 -0400

    Made a Listener, nothing inside

---
+++ src/player/Listener.java
@@ -0,0 +1,184 @@
+package player;
+
+import grammar.ABCMusicBaseListener;
+import grammar.ABCMusicParser;
+
+import java.util.Stack;
+
+public class Listener extends ABCMusicBaseListener {
+    private Stack<MusicPiece> stack = new Stack<MusicPiece>();
+
+    @Override
+    public void exitNote_length_strict(ABCMusicParser.Note_length_strictContext ctx) {
+    }
+
+    @Override
+    public void exitAbc_music(ABCMusicParser.Abc_musicContext ctx) {
+    }
+
+    @Override
+    public void exitOther_fields(ABCMusicParser.Other_fieldsContext ctx) {
+    }
+
+    @Override
+    public void exitAbc_header(ABCMusicParser.Abc_headerContext ctx) {
+    }
+
+    @Override
+    public void exitMulti_note(ABCMusicParser.Multi_noteContext ctx) {
+    }
+
+    @Override
+    public void exitTempo(ABCMusicParser.TempoContext ctx) {
+    }
+
+    @Override
+    public void exitNote_element(ABCMusicParser.Note_elementContext ctx) {
+    }
+
+    @Override
+    public void exitBasenote(ABCMusicParser.BasenoteContext ctx) {
+    }
+
+    @Override
+    public void exitNote_length(ABCMusicParser.Note_lengthContext ctx) {
+    }
+
+    @Override
+    public void exitAbc_tune(ABCMusicParser.Abc_tuneContext ctx) {
+    }
+
+    @Override
+    public void exitField_key(ABCMusicParser.Field_keyContext ctx) {
+    }
+
+    @Override
+    public void exitMeter(ABCMusicParser.MeterContext ctx) {
+    }
+
+    @Override
+    public void exitMeter_fraction(ABCMusicParser.Meter_fractionContext ctx) {
+    }
+
+    @Override
+    public void exitAccidental(ABCMusicParser.AccidentalContext ctx) {
+    }
+
+    @Override
+    public void exitTuplet_element(ABCMusicParser.Tuplet_elementContext ctx) {
+    }
+
+    @Override
+    public void exitKey(ABCMusicParser.KeyContext ctx) {
+    }
+
+    @Override
+    public void exitNote(ABCMusicParser.NoteContext ctx) {
+    }
+
+    @Override
+    public void exitKeynote(ABCMusicParser.KeynoteContext ctx) {
+    }
+
+    @Override
+    public void exitField_tempo(ABCMusicParser.Field_tempoContext ctx) {
+    }
+
+    @Override
+    public void exitElement(ABCMusicParser.ElementContext ctx) {
+    }
+
+    @Override
+    public void exitLyrical_element(ABCMusicParser.Lyrical_elementContext ctx) {
+    }
+
+    @Override
+    public void exitMid_tune_field(ABCMusicParser.Mid_tune_fieldContext ctx) {
+    }
+
+    @Override
+    public void exitEnd_of_line(ABCMusicParser.End_of_lineContext ctx) {
+    }
+
+    @Override
+    public void exitAbc_line(ABCMusicParser.Abc_lineContext ctx) {
+    }
+
+    @Override
+    public void exitTuplet_spec(ABCMusicParser.Tuplet_specContext ctx) {
+    }
+
+    @Override
+    public void exitField_composer(ABCMusicParser.Field_composerContext ctx) {
+    }
+
+    @Override
+    public void exitOctave(ABCMusicParser.OctaveContext ctx) {
+    }
+
+    @Override
+    public void exitNote_or_rest(ABCMusicParser.Note_or_restContext ctx) {
+    }
+
+    @Override
+    public void exitBarline(ABCMusicParser.BarlineContext ctx) {
+    }
+
+    @Override
+    public void exitLyric(ABCMusicParser.LyricContext ctx) {
+    }
+
+    @Override
+    public void exitField_default_length(ABCMusicParser.Field_default_lengthContext ctx) {
+    }
+
+    @Override
+    public void exitKey_accidental(ABCMusicParser.Key_accidentalContext ctx) {
+    }
+
+    @Override
+    public void exitMode_minor(ABCMusicParser.Mode_minorContext ctx) {
+    }
+
+    @Override
+    public void exitField_meter(ABCMusicParser.Field_meterContext ctx) {
+    }
+
+    @Override
+    public void exitField_number(ABCMusicParser.Field_numberContext ctx) {
+    }
+
+    @Override
+    public void exitRest(ABCMusicParser.RestContext ctx) {
+    }
+
+    @Override
+    public void exitComment(ABCMusicParser.CommentContext ctx) {
+    }
+
+    @Override
+    public void exitField_title(ABCMusicParser.Field_titleContext ctx) {
+    }
+
+    @Override
+    public void exitPitch(ABCMusicParser.PitchContext ctx) {
+    }
+
+    @Override
+    public void exitNth_repeat(ABCMusicParser.Nth_repeatContext ctx) {
+    }
+
+    @Override
+    public void exitField_voice(ABCMusicParser.Field_voiceContext ctx) {
+    }
+
+    /**
+     * Gives the final MusicPiece object made from the file inputted
+     * 
+     * @return MusicPiece
+     */
+    protected MusicPiece getMusic() {
+        return stack.get(0);
+    }
+
+}
--- src/player/Main.java
+++ src/player/Main.java
@@ -1,5 +1,16 @@
 package player;
 
+import grammar.ABCMusicLexer;
+import grammar.ABCMusicParser;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RuleContext;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 /**
  * Main entry point of your application.
  */
@@ -15,7 +26,27 @@ public class Main {
      * @param file the name of input abc file
      */
     public static void play(String file) {
-        // YOUR CODE HERE
+        // Create a stream of tokens using the lexer.
+        CharStream stream = new ANTLRInputStream(file);
+        ABCMusicLexer lexer = new ABCMusicLexer(stream);
+        lexer.reportErrorsAsExceptions();
+        TokenStream tokens = new CommonTokenStream(lexer);
+
+        // Feed the tokens into the parser.
+        ABCMusicParser parser = new ABCMusicParser(tokens);
+        parser.reportErrorsAsExceptions();
+
+        // Generate the parse tree using the starter rule.
+        ParseTree tree;
+        tree = parser.abc_tune(); // "abc_tune" is the starter rule.
+        ((RuleContext) tree).inspect(parser);
+
+        // Walk the tree with the listener.
+        ParseTreeWalker walker = new ParseTreeWalker();
+        Listener listener = new Listener();
+        walker.walk(listener, tree);
+        MusicPiece music = listener.getMusic();
+
     }
 
     public static void main(String[] args) {
