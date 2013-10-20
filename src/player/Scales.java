6034c07f02d2f7119d613fcb732256531e1e0a07
Zeke Schmois <schmois@mit.edu>
2013-10-19 20:59:37 -0400

    new Fraction class available. Pitch is a bit different now but still needs work transforming lengths to Fraction. Scales is still in the works

---
+++ src/player/Scales.java
@@ -0,0 +1,20 @@
+package player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Scales {
+
+	private final Map<String, String> SCALES = new HashMap<String, String>(
+			'Cm'=>'c';
+			
+			);
+	private String scale;
+
+	public Scales(String key) {
+		
+		
+		
+		return []
+	}
+}
