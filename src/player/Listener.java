a9d948d6246eaecc51cee421096d44b4191d4bac
Olga Shestopalova <olgash@mit.edu>
2013-10-21 16:55:56 -0400

    Add LyricListener

--- src/player/Listener.java
+++ src/player/Listener.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
-import lyricsParse.LyricsListener;
-
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
---
+++ src/player/LyricsListener.java
@@ -0,0 +1,25 @@
+package player;
+
+import grammar.ABCMusicBaseListener;
+
+import java.util.Stack;
+
+/**
+ * Walks the tree, creates a Lyric object
+ * 
+ * Lyric() return the final Lyric
+ * 
+ */
+public class LyricsListener extends ABCMusicBaseListener {
+    private Stack<Object> stack = new Stack<Object>();
+
+    /**
+     * Gives the final MusicPiece object made from the file inputted
+     * 
+     * @return MusicPiece
+     */
+    public Lyric getLyric() {
+        return (Lyric) stack.get(0);
+    }
+
+}
