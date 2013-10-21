c18f996f7aa5389cc39c0c2d50229d11f46206ab
Zeke Schmois <schmois@mit.edu>
2013-10-21 16:36:11 -0400

    Created and started tests for the MusicSymbol interface and the Utils. Did not finish them and should be a priority but will start writing the second parser for lyrics for now

--- src/sound/LyricListener.java
+++ src/sound/LyricListener.java
@@ -4,9 +4,11 @@ package sound;
  * A LyricListener responds to lyric events.
  */
 public interface LyricListener {
-    /**
-     * This method is called when a lyric event is triggered.
-     * @param text The text associated with the lyric event.
-     */
-    public void processLyricEvent(String text);
+	/**
+	 * This method is called when a lyric event is triggered.
+	 * 
+	 * @param text
+	 *            The text associated with the lyric event.
+	 */
+	public void processLyricEvent(String text);
 }
