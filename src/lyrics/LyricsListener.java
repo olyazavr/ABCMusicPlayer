f24c8d3abcd338f7e17a6afc1ebbc91547ffa2d7
Zeke Schmois <schmois@mit.edu>
2013-10-21 23:51:43 -0400

    adding files missing

---
+++ src/lyrics/LyricsListener.java
@@ -0,0 +1,29 @@
+// Generated from Lyrics.g4 by ANTLR 4.0
+
+package lyrics;
+
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.Token;
+
+public interface LyricsListener extends ParseTreeListener {
+	void enterStar_cluster(LyricsParser.Star_clusterContext ctx);
+	void exitStar_cluster(LyricsParser.Star_clusterContext ctx);
+
+	void enterExtender_cluster(LyricsParser.Extender_clusterContext ctx);
+	void exitExtender_cluster(LyricsParser.Extender_clusterContext ctx);
+
+	void enterLyric(LyricsParser.LyricContext ctx);
+	void exitLyric(LyricsParser.LyricContext ctx);
+
+	void enterMeasure(LyricsParser.MeasureContext ctx);
+	void exitMeasure(LyricsParser.MeasureContext ctx);
+
+	void enterHyphen_cluster(LyricsParser.Hyphen_clusterContext ctx);
+	void exitHyphen_cluster(LyricsParser.Hyphen_clusterContext ctx);
+
+	void enterSyllable_cluster(LyricsParser.Syllable_clusterContext ctx);
+	void exitSyllable_cluster(LyricsParser.Syllable_clusterContext ctx);
+
+	void enterSyllable(LyricsParser.SyllableContext ctx);
+	void exitSyllable(LyricsParser.SyllableContext ctx);
+}
