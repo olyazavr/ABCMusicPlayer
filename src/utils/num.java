189a3833c62bd5d96970a2ef9f6fd1bee734883d
Zeke Schmois <schmois@mit.edu>
2013-10-22 00:58:00 -0400

    More docs on Scales and formatted num. Lyrics.g4 is still ongoing

--- src/utils/num.java
+++ src/utils/num.java
@@ -1,15 +1,21 @@
 package utils;
 
 public class num {
-	public static int gcd(int a, int b){
-		if (a==0) {return b;};
-	    if (a<b) return gcd(b,a);
-	    if (a%b==0) return b;
-	    else return gcd(a, a%b);
+	public static int gcd(int a, int b) {
+		if (a == 0) {
+			return b;
+		}
+		if (a < b) {
+			return gcd(b, a);
+		}
+		if (a % b == 0) {
+			return b;
+		} else {
+			return gcd(a, a % b);
+		}
 	}
 
-
-	public static int lcm(int a, int b){
-	    return ((a*b)/gcd(a,b));
-	} 
+	public static int lcm(int a, int b) {
+		return ((a * b) / gcd(a, b));
+	}
 }
