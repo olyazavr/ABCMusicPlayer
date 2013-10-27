ca4f982175cd1d4e743f6c45ce0051ed8f8d9dce
Zeke Schmois <schmois@mit.edu>
2013-10-27 16:26:09 -0400

    Major changes done to Main method. I migrated most of the methods to a new file called Play and also added a pop up menu that will allow the user to pick which song to play

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -2,10 +2,10 @@ package tests;
 
 import org.junit.Test;
 
-import player.Main;
+import player.Play;
 
 /**
- * Tests all the sample_abc files by playing them in the Main method, only makes
+ * Tests all the sample_abc files by playing them in the Play method, only makes
  * sure they don't crash. These files have all kinds of crazy stuff (chords,
  * repeats, comments, lyrics, etc)
  * 
@@ -20,102 +20,59 @@ public class PlayerTest {
 
 	@Test
 	public void playabcTest() {
-		Main.play("sample_abc/abc_song.abc");
+		Play.play("sample_abc/abc_song.abc");
 	}
 
 	@Test
 	public void fur_eliseTest() {
-		Main.play("sample_abc/fur_elise.abc");
-	}
-
-	@Test
-	public void fur_elise_partTest() {
-		Main.play("sample_abc/fur_elise_part.abc");
-	}
-
-	@Test
-	public void sample_repeat_Test() {
-		Main.play("sample_abc/sample_repeat.abc");
+		Play.play("sample_abc/fur_elise.abc");
 	}
 
 	@Test
 	public void inventionTest() {
-		Main.play("sample_abc/invention.abc");
+		Play.play("sample_abc/invention.abc");
 	}
 
 	@Test
 	public void little_night_musicTest() {
-		Main.play("sample_abc/little_night_music.abc");
+		Play.play("sample_abc/little_night_music.abc");
 	}
 
 	@Test
 	public void paddyTest() {
-		Main.play("sample_abc/paddy.abc");
+		Play.play("sample_abc/paddy.abc");
 	}
 
 	@Test
 	public void piece1Test() {
-		Main.play("sample_abc/piece1.abc");
+		Play.play("sample_abc/piece1.abc");
 	}
 
 	@Test
 	public void piece2Test() {
-		Main.play("sample_abc/piece2.abc");
+		Play.play("sample_abc/piece2.abc");
 	}
 
 	@Test
 	public void piece3Test() {
-		Main.play("sample_abc/piece3.abc");
+		Play.play("sample_abc/piece3.abc");
 	}
 
 	@Test
 	public void preludeTest() {
-		Main.play("sample_abc/prelude.abc");
+		Play.play("sample_abc/prelude.abc");
 	}
 
 	@Test
 	public void scaleTest() {
-		Main.play("sample_abc/scale.abc");
+		Play.play("sample_abc/scale.abc");
 	}
 
 	@Test
 	public void waxies_dargleTest() {
-		Main.play("sample_abc/waxies_dargle.abc");
+		Play.play("sample_abc/waxies_dargle.abc");
 	}
 
-	@Test
-	public void finalFantasyTest() {
-		Main.play("sample_abc/final_fantasy_III.abc");
-	}
 
-	@Test
-	public void dreamOnMusicTest() {
-		Main.play("songs_abc/dreamOnMusic.abc");
-	}
-
-	@Test
-	public void heyJudeTest() {
-		Main.play("sample_abc/heyJude.abc");
-	}
-	
-	@Test
-	public void starwarsTest() {
-		Main.play("songs_abc/starwars2.abc");
-	}
-	
-	@Test
-	public void starTrekTest() {
-		Main.play("songs_abc/starTreklute.abc");
-	}
-	
-	@Test
-	public void barbieGirlTest() {
-		Main.play("songs_abc/aquabarb.abc");
-	}
-	
-	@Test
-	public void fugeTest() {
-		Main.play("songs_abc/fuge.abc");
-	}
 
 }
